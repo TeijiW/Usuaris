4a9ecf21d416b19b8321614cca5d505a10053cfb
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-07-03 17:14:31 -0400

    Adding actions for accounts context

--- lib/usuaris/accounts/accounts.ex
+++ lib/usuaris/accounts/accounts.ex
@@ -2,9 +2,11 @@ defmodule Usuaris.Accounts do
   @moduledoc """
   Accounts context module
   """
-  alias Usuaris.{Account, Accounts, Integrations}
+  alias Usuaris.Accounts
 
+  defdelegate list(params), to: Accounts.List, as: :call
   defdelegate create(params), to: Accounts.Create, as: :call
   defdelegate get(id), to: Accounts.Get, as: :call
+  defdelegate delete(id), to: Accounts.Delete, as: :call
   defdelegate update(id, params), to: Accounts.Update, as: :call
 end
