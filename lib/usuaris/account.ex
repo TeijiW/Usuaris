128634704fb5ed7bfb7f93b419867b7201ba6122
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-07-03 16:45:35 -0400

    Ignore cpf update

--- lib/usuaris/account.ex
+++ lib/usuaris/account.ex
@@ -19,8 +19,18 @@ defmodule Usuaris.Account do
     account
     |> cast(attrs, @fields)
     |> cast_embed(:address, required: true, with: &Address.changeset/2)
+    |> ignore_cpf_when_update()
     |> validate_cpf(:cpf)
     |> unique_constraint(:cpf)
     |> validate_required(@fields)
+  end
+
+  defp ignore_cpf_when_update(changeset) do
+    cpf_update = get_change(changeset, :cpf, nil)
+    current_cpf = changeset.data.cpf
+
+    if cpf_update != current_cpf && not is_nil(current_cpf),
+      do: put_change(changeset, :cpf, current_cpf),
+      else: changeset
   end
 end
