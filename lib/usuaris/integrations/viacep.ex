f3f51acaae13b3af0d93f9d29d55910c6937173f
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-07-03 01:53:18 -0400

    viacep integration

---
+++ lib/usuaris/integrations/viacep.ex
@@ -0,0 +1,20 @@
+defmodule Usuaris.Integrations.Viacep do
+  use Tesla
+  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br"
+  plug Tesla.Middleware.JSON
+
+  def get_address_by_postal_code(postal_code) do
+    get("/ws/#{postal_code}/json") |> handle_response()
+  end
+
+  defp handle_response({:ok, %Tesla.Env{body: %{"erro" => "true"}}}),
+    do: {:error, :not_found}
+
+  defp handle_response({:ok, %Tesla.Env{status: 400}}),
+    do: {:error, :invalid}
+
+  defp handle_response({:ok, %Tesla.Env{status: 200, body: body}}),
+    do: {:ok, body}
+
+  defp handle_response(response), do: response
+end
--- mix.exs
+++ mix.exs
@@ -37,9 +37,11 @@ defmodule Usuaris.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
+      {:hackney, "~> 1.17"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
+      {:tesla, "~> 1.4"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
