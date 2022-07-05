1d782b3f8b07a6e51e274f2761041442fc3e28da
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-07-05 01:25:34 -0400

    Accounts controller show action tests cases

--- test/usuaris_web/controllers/accounts_controller_test.exs
+++ test/usuaris_web/controllers/accounts_controller_test.exs
@@ -36,4 +36,22 @@ defmodule UsuarisWeb.AccountsControllerTest do
       assert length(response) == 2
     end
   end
+
+  describe "Get details from an account" do
+    test "with an account that exists", %{conn: conn} do
+      inserted_account = insert(:account)
+
+      assert response =
+               conn |> get(accounts_path(conn, :show, inserted_account.id)) |> json_response(200)
+
+      assert response["id"] == inserted_account.id
+      assert response["name"] == inserted_account.name
+      assert response["address"]["postal_code"] == inserted_account.address.postal_code
+    end
+
+    test "with an account that not exists", %{conn: conn} do
+      assert response = conn |> get(accounts_path(conn, :show, 123)) |> json_response(404)
+      assert response["status"] == "Not found"
+    end
+  end
 end
